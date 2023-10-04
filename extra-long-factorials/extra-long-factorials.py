f29817076782d430e5cffcecc2212bb5df8dcf23
Rodrigo Donizetti de Oliveira <rodrigodonizettio@gmail.com>
2023-10-04 08:35:06 -0300

    [Challenge Solution] Extra Long Factorials

---
+++ extra-long-factorials/extra-long-factorials.py
@@ -0,0 +1,14 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+def extraLongFactorials(n):
+    print(math.factorial(n))
+
+
+if __name__ == '__main__':
+    n = 100
+    extraLongFactorials(n)
